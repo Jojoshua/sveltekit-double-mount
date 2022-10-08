3e33c2d4b704d9c3e436a1cad15779f9d96f59d8
Jojoshua <jojoshua@gmail.com>
2022-10-08 16:12:02 -0400

    HMR private env observation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import App from '../lib/App.svelte';
+	//Start dev, then uncomment the following 2 lines and save
+	//import { env as private_env } from '$env/dynamic/private';
+	//console.log(private_env);
+
 </script>
 
 <h1>Welcome to your library project</h1>
